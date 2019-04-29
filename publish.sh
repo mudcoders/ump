dff660d5ca269c79337125c46f84ce8399013eff
Graham Cox <graham@grahamcox.co.uk>
2019-04-29 21:23:25 +0100

    Try publishing with gh-pages instead

--- publish.sh
+++ publish.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
 set -e
 
-# Create a working area
-TEMP_DIR="$(mktemp -q -d -t "$(basename "$0").XXXXXX" 2>/dev/null || mktemp -q -d)"
-
 # Build the book
 npm install
-mv _book/* $TEMP_DIR
-rm -rf _book
 
-# Check out the target branch
-git branch -l | grep gh-pages && git branch -D gh-pages
-git checkout origin/gh-pages
-git checkout -b gh-pages
-
-# Wipe the old commit out
-rm -rf *
-git add .
-
-# Copy the new version in
-mv $TEMP_DIR/* .
-git add .
-
-# Commit the latest version
-git commit -m "Built site"
-
-# Wipe out the working area
-rm -rf $TEMP_DIR
-
-# Push the changes
-if [ ! -z "$GH_TOKEN" ]; then
-    git push https://${GH_TOKEN}:x-oauth-basic@github.com/mudcoders/ump.git gh-pages
-fi
+# And publish it
+npm install -g gh-pages
+gh-pages -d _book
